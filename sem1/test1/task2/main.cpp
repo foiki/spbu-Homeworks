f76e16a371958e6472b0ea85709f84c3ab52052d
Ilya Kireev <ilyakireev53@mail.ru>
2018-11-02 15:59:31 +0300

    Correction of Test1 task2

--- sem1/test1/task2/main.cpp
+++ sem1/test1/task2/main.cpp
@@ -1,74 +1,43 @@
 #include <iostream>
-#include <ctime>
+#include <fstream>
 
 using namespace std;
 
-void randomMembers(int a[], int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        a[i] = rand() % 33 + 11;
-    }
-}
-
-void read(int a[], int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
-}
-
-void print(int a[], int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-}
-
-void insertionSort(int a[], int size)
-{
-    for (int i = 3; i < size; i += 2)
-    {
-        int key = a[i];
-        int j = i - 2;
-        while (j >= 0 && a[j] > key)
-        {
-            a[j + 2] = a[j];
-            j -= 2;
-        }
-        a[j + 2] = key;
-    }
-}
-
 int main()
 {
-    srand(time(0));
-    const int maxSize = 1000000;
-    int array[maxSize];
-    int number = 0;
-    cout << "Enter the size of array: ";
-    cin >> number;
-    cout << "If you want to enter the members of array by yourself, enter '0', if you want to generate them, enter '1'" << endl;
-    bool random = false;
-    cin >> random;
-    if (random)
+    ifstream fin("input.txt");
+    const int maxSize = 1000;
+    int array[maxSize] {0};
+    if (fin.is_open())
     {
-        randomMembers(array, number);
-        cout << "Generated Array: " << endl;
-        print(array, number);
+        while (!fin.eof())
+        {
+            char letter =' ';
+            fin >> letter;
+            if (letter >= 'a' && letter <= 'z')
+            {
+                ++array[letter];
+            }
+        }
+        ofstream fout("output.txt");
+        if (fout.is_open())
+        {
+            for (int i = 'a'; i <= 'z'; ++i)
+            {
+                if (array[i] > 0)
+                {
+                    fout << char(i) << " " << array[i] << endl;
+                }
+            }
+        }
+        else
+        {
+            cout << "Output file not found!" << endl;
+        }
     }
     else
     {
-        cout << "Enter the array members: ";
-        read(array, number);
-        cout << "Entered Array: " << endl;
-        print(array, number);
+        cout << "Input file not found!" << endl;
     }
-    insertionSort(array, number);
-    cout << "Result: " << endl;
-    print(array, number);
     return 0;
 }
