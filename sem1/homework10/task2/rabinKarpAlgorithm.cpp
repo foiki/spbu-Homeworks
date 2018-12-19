cdab372759914187517d7d274425efe23f4c55fe
Ilya Kireev <ilyakireev53@mail.ru>
2018-12-19 22:00:55 +0300

    Fixed algorithm

--- sem1/homework10/task2/rabinKarpAlgorithm.cpp
+++ sem1/homework10/task2/rabinKarpAlgorithm.cpp
@@ -1,15 +1,21 @@
 #include "rabinKarpAlgorithm.hpp"
 
 const int intMax = 2147483647;
+const int prime = 13;
+
+void countNewHashOfString(string line, long length, long &hash)
+{
+    hash -= (line[0] * prime) % intMax;
+    hash += (line[length - 1] * prime) % intMax;
+}
 
 int hashFunction(string line)
 {
-    int const prime = 13;
     int result = 0;
     long length = line.length();
     for (int i = 0; i < length; ++i)
     {
-        result = ((result + line[i]) * prime) % intMax;
+        result = (result + line[i] * prime) % intMax;
     }
     return result;
 }
@@ -21,10 +27,15 @@ bool rabinKarpAlgorithm(string line, string subLine)
     long secondLength = subLine.length();
     cout << "First indices of all occurrences of a subline in a line:" << endl;
     bool isOccurrenceFound = false;
+    string newSubLine = line.substr(0, secondLength);
+    long newSubLineHash = hashFunction(newSubLine);
     for (long i = 0; i < firstLength - secondLength + 1; ++i)
     {
-        string newSubLine = line.substr(i, secondLength);
-        long newSubLineHash = hashFunction(newSubLine);
+        if (i != 0)
+        {
+            countNewHashOfString(line.substr(i - 1, secondLength + 1), secondLength + 1, newSubLineHash);
+        }
+        newSubLine = line.substr(i, secondLength);
         if (subLineHash == newSubLineHash)
         {
             if (newSubLine == subLine)
