4b60e6f7476b32f0e523d9b5dae890367e26c428
Ilya Kireev <ilyakireev53@mail.ru>
2019-03-02 21:02:10 +0300

    Fixed .idea file adding and pop method exception

--- sem2/homework1/task1/src/group144/kireev/Stack.java
+++ sem2/homework1/task1/src/group144/kireev/Stack.java
@@ -1,74 +1,42 @@
 package group144.kireev;
 import java.util.Scanner;
 
-/**
- *A class that realizes ordinary stack
- */
-
+/** A class that realizes ordinary stack */
 public class Stack {
-    private class StackElement {
-        private int value;
-        private StackElement next;
-
-        public StackElement(int value, StackElement next) {
-            this.value = value;
-            this.next = next;
-        }
-
-        public int getValue() {
-
-            return value;
-        }
-
-        public StackElement getNext() {
-
-            return next;
-        }
-    }
 
     private StackElement top;
+    private int length;
 
-    /**
-     *A method that adds element to the stack
-     */
-
-    public void add(int value) {
+    /** A method that adds element to the stack */
+    public void push(int value) {
 
         top = new StackElement(value, top);
+        ++length;
     }
 
-    /**
-     *A method that deletes top element from the stack
-     */
-
-    public void pop() {
-
-        top = top.getNext();
-    }
-
-    /**
-     *A method that returns the size of stack
-     */
-
-    public int getSize() {
-        StackElement current = top;
-        int result = 0;
-        while (current != null) {
-            ++result;
-            current = current.getNext();
+    /** A method that deletes top element from the stack */
+    public void pop() throws NullPointerException {
+        if (top == null) {
+            throw new NullPointerException("Nothing to delete!");
         }
-        return result;
+        top = top.next;
+        --length;
     }
 
-    /**
-     *A method that prints the stack
-     */
+    /** A method that returns the size of stack */
+    public int getSize() {
+        return length;
+    }
 
-    public void printStack() {
+    /** A method that prints the stack */
+    public void printStack() throws NullPointerException {
+        if (top == null) {
+            throw new NullPointerException("No elements in the stack!");
+        }
         StackElement current = top;
         while (current != null) {
-            System.out.print(current.getValue() + " ");
-            current = current.getNext();
+            System.out.print(current.value + " ");
+            current = current.next;
         }
         System.out.println();
     }
@@ -89,13 +57,21 @@ public class Stack {
                 case 1:
                     System.out.print("Enter new element to add: ");
                     int newElement = in.nextInt();
-                    stack.add(newElement);
+                    stack.push(newElement);
                     break;
                 case 2:
-                    stack.pop();
+                    try {
+                        stack.pop();
+                    } catch (NullPointerException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 3:
-                    stack.printStack();
+                    try {
+                        stack.printStack();
+                    } catch (NullPointerException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 4:
                     System.out.println("The Stack size is: " + stack.getSize());
@@ -109,6 +85,16 @@ public class Stack {
                     System.out.println("Enter '5' to see help menu");
                     break;
             }
+        }
+    }
+
+    private class StackElement {
+        private int value;
+        private StackElement next;
+
+        public StackElement(int value, StackElement next) {
+            this.value = value;
+            this.next = next;
         }
     }
 }
