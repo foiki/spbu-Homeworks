20ae6d2195584a3ebf0ed29d818c0c82bb8cee5d
Ilya Kireev <ilyakireev53@mail.ru>
2019-05-04 20:14:31 +0300

    Minor fixes

--- sem2/homework5/task1/src/main/java/group144/kireev/Main.java
+++ sem2/homework5/task1/src/main/java/group144/kireev/Main.java
@@ -5,17 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Slider and Progress Bar");
-        Scene scene = new Scene(root, 600, 600);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            primaryStage.setTitle("Slider and Progress Bar");
+            Scene scene = new Scene(root, 600, 600);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+    }
     }
 
     public static void main(String[] args) {
