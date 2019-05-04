b646327c5ec4570827ad9da2d16758d71c6459b0
Ilya Kireev <ilyakireev53@mail.ru>
2019-05-04 22:31:22 +0300

    Fixed exception in main method

--- sem2/homework5/task2/src/main/java/group144/kireev/Main.java
+++ sem2/homework5/task2/src/main/java/group144/kireev/Main.java
@@ -5,16 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle("Simple Calculator");
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            primaryStage.setTitle("Simple Calculator");
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
