1cea8678df70c0e0d921739351dbf0ab870f8abe
Ilya Kireev <ilyakireev53@mail.ru>
2020-03-06 17:30:58 +0300

    Fixed bug with incorrect index

--- sem4/homework1/task2/fibonacci.hs
+++ sem4/homework1/task2/fibonacci.hs
@@ -3,4 +3,4 @@ module Fibonacci where
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+fibonacci n = if n >= 2 then fibonacci (n - 1) + fibonacci (n - 2) else error "Incorrect index"
