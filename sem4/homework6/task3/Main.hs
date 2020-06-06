bf4d3333d76aee10cf9abf3f17502d2f85866614
Ilya Kireev <ilyakireev53@mail.ru>
2020-06-06 12:42:49 +0300

    Fixed task

--- sem4/homework6/task3/Main.hs
+++ sem4/homework6/task3/Main.hs
@@ -1,50 +1,40 @@
 import System.IO
 import Control.Monad
-import Data.Map
-import Data.Function(on)
-import Data.List(sortBy)
+import Data.List
+import Data.Maybe
 
-data Graph v = Graph [(Int,v)] [(Int,Int,Int)] deriving (Show)
+data Graph = Graph [Int] [((Int,Int),Double)] deriving (Show)
+data Path = Path {
+    from :: Int,
+    to :: Int,
+    dist :: Double
+} deriving (Eq)
 
-graph1 = Graph [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)] [(1, 2, 10), (1, 3, 7), (3, 4, 4), (2, 3, 8), (3, 5, 6), (1, 5, 23)]
-graph2 = Graph [(1, 1), (2, 2), (3, 3)] [(1, 2, 1), (1, 3, 3), (2, 3, 2)]
+instance Ord Path where
+    compare p1 p2 = dist p1 `compare` dist p2
+
+instance Show Path where
+    show path = show (to path, dist path)
+
+graph1 = Graph [1..5] [((1, 2), 10), ((1, 3), 7), ((3, 4), 4), ((2, 3), 8), ((3, 5), 6), ((1, 5), 23)]
+graph2 = Graph [1..3] [((1, 2), 1), ((1, 3), 3), ((2, 3), 2)]
 
 main = do
     hSetBuffering stdin LineBuffering
     putStrLn "graph1, start point = 1:"
-    putStrLn "vertex, minLenght: path"
-    printResults $ dijkstra graph1 1
+    print $ dijkstra graph1 1
     putStrLn "graph2, start point = 1:"
-    putStrLn "vertex, minLenght: path"
-    printResults $ dijkstra graph2 1
+    print $ dijkstra graph2 1
     
+    
+dijkstra:: Graph -> Int -> [Path]
+dijkstra (Graph vertexes paths) src =  dijkstra' (map (\vertex -> Path vertex vertex (if vertex == src then 0.0 else 1 / 0)) vertexes) [] where
+    dijkstra' [] visited = visited
+    dijkstra' notVisited visited = dijkstra' closestNotVis updatedVisited where
+        closest = minimum notVisited
+        updatedVisited = closest : visited
+        closestNotVis = (map (\p -> min p (getNewPath closest p)) . delete closest) notVisited
+        getNewPath (Path closest _ nearestDist) (Path currNode _ _) = Path currNode closest (nearestDist + distBetweenNodes closest currNode) 
+        distBetweenNodes x y = fromJust $ Prelude.lookup (x, y) paths `mplus` Just (1 / 0)
 
-dijkstra:: Graph v -> Int -> [(Int, (Int, [Int]))]
-dijkstra (Graph vertexes paths) src = toList $ updateVisited (Prelude.map fst vertexes) paths $ insert src (0, [src]) empty
-
-printResults:: [(Int, (Int, [Int]))] -> IO ()
-printResults [] = return ()
-printResults ((x,(length,path)):xs) = do 
-    putStr $ show x ++ ", " ++ show length ++ ": "
-    putStrLn $ Prelude.foldr (\x -> (++) (show x ++ " ")) "" path
-    printResults xs
-
-updateVisited:: [Int] -> [(Int, Int, Int)] -> Map Int (Int, [Int]) -> Map Int (Int, [Int])
-updateVisited [] _ map = map
-updateVisited notVisited@(x:_) paths map = updateVisited updatedNotVisited paths (updateWeights updatedNotVisited vertex paths map) where
-    orderedVertexes = sortBy (compare `on` fst . snd) $ toList map
-    filteredVertexes = Prelude.filter (`elem` notVisited) $ Prelude.map fst orderedVertexes
-    vertex = if Prelude.null filteredVertexes then x else head filteredVertexes
-    updatedNotVisited = Prelude.filter (/=vertex) notVisited
-
-updateWeights:: [Int] -> Int -> [(Int, Int, Int)] -> Map Int (Int, [Int]) -> Map Int (Int, [Int])
-updateWeights _ _ [] vertexMap = vertexMap
-updateWeights notVisited src ((x,y,pathLength):xs) vertexMap
-    | src == x && elem y notVisited = updateWeight notVisited src ((x,y,pathLength):xs) vertexMap
-    | otherwise = updateWeights notVisited src xs vertexMap
-
-updateWeight:: [Int] -> Int -> [(Int, Int, Int)] -> Map Int (Int, [Int]) -> Map Int (Int, [Int])
-updateWeight notVisited src ((x,y,pathLength):xs) vertexMap = case (Data.Map.lookup x vertexMap, Data.Map.lookup y vertexMap) of
-    (Just (len, path), Just (minLength, _)) | len + pathLength < minLength -> updateWeights notVisited src xs $ insert y (len + pathLength, path ++[y]) vertexMap
-    (Just (len,path), Nothing) -> updateWeights notVisited src xs $ Data.Map.insert y (len + pathLength, path ++[y]) vertexMap
-    _ -> updateWeights notVisited src xs vertexMap
+    
