5eca862877561bdc4ede30a04c6c1829784a891d
Ilya Kireev <ilyakireev53@mail.ru>
2021-03-18 14:52:29 +0300

    Fix method name

--- sem6/calcPract/src/main/kotlin/homework3/PowerMethod.kt
+++ sem6/calcPract/src/main/kotlin/homework3/PowerMethod.kt
@@ -3,7 +3,7 @@ package homework3
 class PowerMethod(matrix: Array<Array<Double>>, eps: Double):
     MaxAbsEigenValueMethod(matrix, eps) {
 
-    override var methodName = "scalar product"
+    override var methodName = "power-law"
 
     override fun updateMaxAbsEigenValue(newYK: Array<Double>, eigenVector: Array<Double>): Double {
         return newYK[0] / eigenVector[0]
