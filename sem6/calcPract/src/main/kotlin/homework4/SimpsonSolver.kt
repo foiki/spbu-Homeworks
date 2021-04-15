98b6f49bb6111103ccef178c52860fbc71e67ba3
Ilya Kireev <ilyakireev53@mail.ru>
2021-04-15 13:10:56 +0300

    Add task4

---
+++ sem6/calcPract/src/main/kotlin/homework4/SimpsonSolver.kt
@@ -0,0 +1,40 @@
+package homework4
+
+import kotlin.math.abs
+
+class SimpsonSolver(val a: Double, val b: Double) {
+
+    private var h: Double = 1.0
+    private var points: Array<Pair<Double, Double>> = arrayOf()
+
+    fun solve(x: Double, N: Int): Array<Double> {
+        h = abs(b - a) / N
+        initPoints(x, 2 * N)
+        return calculateCoefficients(N)
+    }
+
+    private fun initPoints(x: Double, N: Int) {
+        points = arrayOf()
+        for (i in 0..N) {
+            val xI = a + i * h
+            val fI = H(x, xI)
+            points += Pair(xI, fI)
+        }
+    }
+
+    private fun calculateCoefficients(N: Int): Array<Double> {
+        var coefficients: Array<Double> = arrayOf()
+        val div = (b - a) / (6 * N)
+        coefficients += div * points[0].second
+        for (i in 1 until 2 * N) {
+            val multi = if (i % 2 == 0) {
+                2
+            } else {
+                4
+            }
+            coefficients += div * multi * points[i].second
+        }
+        coefficients += div * points[2 * N].second
+        return coefficients
+    }
+}
